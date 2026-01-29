8962d416223d4ff7f012827d670f6cfe569cc109
Alvin Le <alvinlester24@gmail.com>
2026-01-28 18:04:34 -0800

    Initial Commit

---
+++ blubble/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  blubble
+//
+//  Created by Alvin Ngoc Le on 1/28/26.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ blubble/blubbleApp.swift
@@ -0,0 +1,33 @@
+//
+//  blubbleApp.swift
+//  blubble
+//
+//  Created by Alvin Ngoc Le on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct blubbleApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
