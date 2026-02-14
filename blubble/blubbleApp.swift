7f1bc3e5e1bfee9f6dafc94cddf0d9a8987d5614
Alvin Le <alvinlester24@gmail.com>
2026-02-13 19:02:29 -0800

    remove redundant tracking view and transcription service

--- blubble/blubbleApp.swift
+++ blubble/blubbleApp.swift
@@ -10,24 +10,14 @@ import SwiftUI
 @main
 struct blubbleApp: App {
 
-    @State private var appModel = AppModel()
-    @State private var voiceSpatialManager = VoiceSpatialManager()
     @State private var conversationStore = ConversationStore()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(appModel)
-                .environment(voiceSpatialManager)
                 .environment(conversationStore)
         }
         .windowStyle(.plain)
         .defaultSize(width: 500, height: 600)
-        
-        ImmersiveSpace(id: "PartnerTracking") {
-            PartnerTrackingView()
-                .environment(voiceSpatialManager)
-        }
-        .immersionStyle(selection: .constant(.mixed), in: .mixed)
     }
 }
